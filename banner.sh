7851c428eb33f24b9919236e4891c013ab5ec752
Aditya Rajput <adiraj20072002@gmail.com>
2024-01-19 19:23:24 +0530

    Create banner.sh

---
+++ banner.sh
@@ -0,0 +1,15 @@
+server_name=""
+url="https://raw.githubusercontent.com/mdgspace/server-map/main/$server_name.md"
+response=$(curl -s "$url")
+
+echo "--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---"
+echo ""
+echo "Hello there! This is the '$server_name' server."
+echo ""
+echo "It currently contains the following projects:"
+echo ""
+echo $response
+echo ""
+echo "If you add/remove a project, please update the server-map repo."
+echo ""
+echo "--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---"
